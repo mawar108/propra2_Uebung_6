5fb2c9bd0faef645c6265f3de1a58d991d772bde
Couraxe <xvertexsun@gmail.com>
2020-12-15 15:30:20 +0100

    Implement berechneEndkapital

--- src/main/java/com/example/demo/Ergebnis.java
+++ src/main/java/com/example/demo/Ergebnis.java
@@ -6,4 +6,20 @@ import java.util.List;
 public class Ergebnis {
     private List<Eintrag> tabelle = new ArrayList<>();
     private double endkapital;
+
+    public void addEintrag(Eintrag e) {
+        tabelle.add(e);
+    }
+
+    public List<Eintrag> getTabelle() {
+        return tabelle;
+    }
+
+    public double getEndkapital() {
+        return endkapital;
+    }
+
+    public void setEndkapital(double endkapital) {
+        this.endkapital = endkapital;
+    }
 }
--- src/main/java/com/example/demo/ZinsService.java
+++ src/main/java/com/example/demo/ZinsService.java
@@ -3,7 +3,21 @@ package com.example.demo;
 
 public class ZinsService {
 
-    public double berechneEndkapital() {
+    public Ergebnis berechneEndkapital(ZinsForm form) {
+        Ergebnis e = new Ergebnis();
+        int laufzeit = form.getLaufzeit();
+        double startKapital = form.getAnfangsKapital();
+        double zins = form.getZinsSatz();
 
+        for (int i = 1; i <= laufzeit; ++i) {
+            double zinsen = startKapital * (zins / 100);
+            Eintrag eintrag = new Eintrag(i, startKapital, zinsen, startKapital + zinsen);
+            startKapital += zinsen;
+            e.addEintrag(eintrag);
+        }
+
+        e.setEndkapital(startKapital);
+
+        return e;
     }
 }
